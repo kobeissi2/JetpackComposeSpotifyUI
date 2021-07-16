178797f01e43cf8b4d9da226bbc88feda58437d1
Moe <mohamadk@umich.edu>
2021-07-16 00:18:54 -0400

    Basic UI changes just to test out the theming

--- app/src/main/java/com/kobeissidev/jetpackcomposedemo/MainActivity.kt
+++ app/src/main/java/com/kobeissidev/jetpackcomposedemo/MainActivity.kt
@@ -3,11 +3,18 @@ package com.kobeissidev.jetpackcomposedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.kobeissidev.jetpackcomposedemo.ui.theme.JetpackComposeDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +24,14 @@ class MainActivity : ComponentActivity() {
             JetpackComposeDemoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Row {
+                        TopAppBar(backgroundColor = Color.Magenta) {
+                            Text(text = "Hello ActionBar")
+                        }
+                        Column(modifier = Modifier.fillMaxSize()) {
+                            Greeting("Android")
+                        }
+                    }
                 }
             }
         }
@@ -26,7 +40,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!", fontSize = 30.sp)
 }
 
 @Preview(showBackground = true)
